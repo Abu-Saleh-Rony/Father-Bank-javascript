ea659b19038317ed7a6448f6e9d182d4d51d82f2
abusalehrony <abusalehrony12@gmail.com>
2022-08-18 16:52:18 +0600

    added portion

--- js/deposit.js
+++ js/deposit.js
@@ -2,6 +2,7 @@ document.getElementById('deposit-Button').addEventListener('click', function () 
 
     const depositField = document.getElementById('deposit-field');
     const newDepositAmountString = depositField.value;
+
     if (newDepositAmountString !== "") {
         console.log(newDepositAmountString);
         const newDepositAmount = parseFloat(newDepositAmountString);
@@ -17,8 +18,16 @@ document.getElementById('deposit-Button').addEventListener('click', function () 
 
         depositTotalElement.innerText = currentDepositTotal;
 
+        const balanceTotalElement = document.getElementById('balance-total');
+        const previousBalanceTotalString = balanceTotalElement.innerText;
+        const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+        console.log(previousBalanceTotal);
+
+        const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+        balanceTotalElement.innerText = currentBalanceTotal;
 
         depositField.value = '';
-
     }
+
 })
+
